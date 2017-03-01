b40f528653ad20235ec229ba3bf1c4ab95bc29dd
Undust4able <t4.user@gmail.com>
2017-03-01 20:16:19 +0300

    do not draw identical boxes

--- app/src/main/java/laiden/fanfiction/DrawThread.java
+++ app/src/main/java/laiden/fanfiction/DrawThread.java
@@ -6,7 +6,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.view.SurfaceHolder;
+
+import java.util.ArrayList;
+
+import laiden.fanfiction.project.Thing;
 
 /**
  * Created by Undust4able on 23.02.2017.
@@ -36,13 +41,18 @@ class DrawThread extends Thread{
             canvas = null;
             try {
                 // получаем объект Canvas и выполняем отрисовку
+                ArrayList<RectF> drawnboxes = new ArrayList<>();
                 canvas = surfaceHolder.lockCanvas(null);
                 if(canvas != null && StoryView.s != null && StoryView.s.things != null) synchronized (surfaceHolder) {
                     canvas.drawColor(Color.parseColor(StoryView.s.background)); // todo: add a resource parser: background can be image or color
 
                     for(int j = StoryView.s.things.size() - 1; j >= 0; j--){
-                        boolean sel = StoryView.s.things.get(j) == StoryView.thing;
-                        StoryView.s.things.get(j).render(canvas, sel);
+                        Thing t = StoryView.s.things.get(j);
+                        boolean sel = t == StoryView.thing;
+                        if(drawnboxes.indexOf(t.box()) == -1){
+                            t.render(canvas, sel);
+                            drawnboxes.add(t.box());
+                        }
                     }
                 }
             }
